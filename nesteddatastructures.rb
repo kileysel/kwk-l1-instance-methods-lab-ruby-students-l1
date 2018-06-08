8e8fd713b2886e8d2b0efaa127ea1e6daf73a4cb
kileysel <kiley@selmanhouse.com>
2018-06-08 20:12:00 +0000

    automatically backed up by learn

---
+++ nesteddatastructures.rb
@@ -0,0 +1,14 @@
+#create a hash with nested arrays of your groups favorite songs 
+
+#create a hash 
+
+group_name = { 
+  #:person_1 => ["fav song 1". "fav song 2 "]
+  :maya => ["1999", "fergalicious", "a whole new world"],
+  :madi => ["watch", "wish", "big face gerbs"],
+  :kiley => ["miami", "who dat boy", "apricot princess"]
+}
+#call the first person second fav song 
+#call the second persons fav first fav song 
+puts group_name [:maya]
+puts group name 
